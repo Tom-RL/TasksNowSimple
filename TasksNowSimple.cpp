fea26e18490aa0cd99d21927d2d562d3d9c3409c
Richard Pierre <richardrosariopierre@gmail.com>
2025-06-20 17:53:40 -0300

    I FINALLY FINISHED TNS
    removeTask() and markTaskAsCompleted() added, I reorganized the accessibility functions and fix bug of IDs in addTask()

--- TasksNowSimple.cpp
+++ TasksNowSimple.cpp
@@ -1,38 +1,48 @@
-#define NOMINMAX
+#define NOMINMAX // disabling min and max from Windows.h header
 #include <limits>
 #include <Windows.h>
 #include <vector>
 #include <iostream>
 #include <chrono>
 #include <thread>
-#include "task/task.h"
-#include "storage/Storage.h"
+#include "task/task.h" // Class Task header
+#include "storage/Storage.h" // Storage functions header
 
 std::vector<Task> taskList{}; //temporário
 
-// Set console text color
-void setColor(WORD color)
+// a local namespace to agroup customizing functions
+namespace customize
 {
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
-}
+	// Set console text color
+	void setColor(WORD color)
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+	}
 
-// Clear console screen
-void clearScreen() 
-{
-	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	CONSOLE_SCREEN_BUFFER_INFO csbi;
-	DWORD count;
-	DWORD cellCount;
-	COORD homeCoords = { 0, 0 };
+	// Clear console screen
+	void clearScreen()
+	{
+		HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+		CONSOLE_SCREEN_BUFFER_INFO csbi;
+		DWORD count;
+		DWORD cellCount;
+		COORD homeCoords = { 0, 0 };
 
-	if (hStdOut == INVALID_HANDLE_VALUE) return;
-	if (!GetConsoleScreenBufferInfo(hStdOut, &csbi)) return;
+		if (hStdOut == INVALID_HANDLE_VALUE) return;
+		if (!GetConsoleScreenBufferInfo(hStdOut, &csbi)) return;
 
-	cellCount = csbi.dwSize.X * csbi.dwSize.Y;
+		cellCount = csbi.dwSize.X * csbi.dwSize.Y;
 
-	FillConsoleOutputCharacter(hStdOut, ' ', cellCount, homeCoords, &count);
-	FillConsoleOutputAttribute(hStdOut, csbi.wAttributes, cellCount, homeCoords, &count);
-	SetConsoleCursorPosition(hStdOut, homeCoords);
+		FillConsoleOutputCharacter(hStdOut, ' ', cellCount, homeCoords, &count);
+		FillConsoleOutputAttribute(hStdOut, csbi.wAttributes, cellCount, homeCoords, &count);
+		SetConsoleCursorPosition(hStdOut, homeCoords);
+	}
+
+	// Set a delayer
+	void setDelay(int milliseconds)
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
+	}
 }
 
 // Checks for input errors and clears the input buffer if necessary	
@@ -46,6 +56,7 @@ void cinError()
 }
 
 void showMenu() //function to show the menu options
+
 {
 	std::cout << "\t\tTask Manager\n\n"
 		      << "1. Add task\n"
@@ -60,17 +71,25 @@ void addTask() //function to add a task
 {
 	static int id{ 0 }; // ID of task, start in 0 and increments with each new task
 
-	std::this_thread::sleep_for(std::chrono::milliseconds(1200));
-	clearScreen();
+	for (const auto& task : taskList)
+	{
+		if (id <= task.getId()) // checks if the id is already used
+		{
+			id = task.getId() + 1; // increments the id to the next available one
+		}
+	}
+
+	customize::setDelay(1200); 
+	customize::clearScreen();
 
 	std::cout << "Enter the task name: ";
 	std::string name{};
 	std::getline(std::cin >> std::ws, name);
 	if (name.empty())
 	{
-		setColor(12); // red
+		customize::setColor(12); // red
 		std::cout << "Invalid name. Task name cannot be empty.\n";
-		setColor(7);
+		customize::setColor(7);
 		return;
 	}
 
@@ -79,42 +98,42 @@ void addTask() //function to add a task
 	std::getline(std::cin >> std::ws, description);
 	if (description.empty())
 	{
-		setColor(12); // red
+		customize::setColor(12); // red
 		std::cout << "Invalid description. Task description cannot be empty.\n";
-		setColor(7);
+		customize::setColor(7);
 		return;
 	}
 
 	taskList.emplace_back(Task{ name, description, id, false });
 	id++; // increments the id for the next task
 
-	setColor(10); // green
+	customize::setColor(10); // green
 	std::cout << "Task added successfully!\n\n";
-	setColor(7);
-	std::this_thread::sleep_for(std::chrono::seconds(2)); 
-	clearScreen();
+	customize::setColor(7);
+	customize::setDelay(2000);
+	customize::clearScreen();
 }
 
 void listTasks() //funtion to list tasks
 {
-	std::this_thread::sleep_for(std::chrono::milliseconds(1200));
-	clearScreen();
+	customize::setDelay(1200);
+	customize::clearScreen();
 	if(taskList.empty())
 	{ 
-		setColor(12); // red
+		customize::setColor(12); // red
 		std::cout << "No tasks available.\n\n";
-		setColor(7);
-		std::this_thread::sleep_for(std::chrono::seconds(2));
-		clearScreen();
+		customize::setColor(7);
+		customize::setDelay(2000);	
+		customize::clearScreen();
 		return;
 	}
 	else
 	{ 
-		setColor(10); // green
+		customize::setColor(10); // green
 		std::cout << "List of tasks:\n\n";
-		setColor(7);
+		customize::setColor(7);
 
-		std::this_thread::sleep_for(std::chrono::seconds(1));
+		customize::setDelay(500); // delay for 0.5 seconds
 		for (const auto& task : taskList)
 		{
 			std::cout << task << "\n\n"; // uses the overloaded operator<< from task.h to print the task
@@ -122,47 +141,176 @@ void listTasks() //funtion to list tasks
 		std::cout << "Press Enter to continue...\n";
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 		std::cin.get();
-		clearScreen();
+		customize::clearScreen();
 	}
 }
 
 void markTaskAsCompleted() //funtion to mark a task as completed
 {
+	customize::setDelay(1200);
+	customize::clearScreen();
+
+	if (taskList.empty())
+	{
+		customize::setColor(12); // red
+		std::cout << "No tasks available to mark as completed.\n\n";
+		customize::setColor(7);
+		customize::setDelay(2000);
+		customize::clearScreen();
+		return;
+	}
+	else
+	{
+		std::cout << "Enter the ID of the task to mark as completed: ";
+		int id{};
+		std::cin >> id;
+		if (!std::cin)
+		{
+			cinError();
+			customize::setColor(12); // red
+			std::cout << "Invalid input. Please enter a valid task ID.\n\n";
+			customize::setColor(7);
+			customize::setDelay(2000);
+			customize::clearScreen();
+			return;
+		}
+		if (id < 0) // checks if the id is negative
+		{
+			customize::setColor(12); // red
+			std::cout << "Invalid ID. Task ID cannot be negative.\n\n";
+			customize::setColor(7);
+			customize::setDelay(2000);
+			customize::clearScreen();
+			return;
+		}
+
+		for (auto& task : taskList) // accesses each task in the task list
+		{
+			if (task.getId() == id) // checks if the task ID matches the input ID
+			{
+				if (task.isCompleted()) // checks if the task is already marked as completed
+				{
+					customize::setColor(12); // red
+					std::cout << "Task " << task.getName() << " is already marked as completed.\n\n";
+					customize::setColor(7);
+					customize::setDelay(2000);
+					customize::clearScreen();
+					return;
+				}
+				else
+					{
+					customize::setColor(14); // yellow
+					std::cout << "Marking task " << task.getName() << " as completed...\n";
+					task.markCompleted(); // marks the task as completed
+					customize::setDelay(1000);
+					customize::clearScreen();
+
+					customize::setColor(10); // green
+					std::cout << "Task " << task.getName() << " marked as completed successfully!\n\n";
+					customize::setColor(7);
+					customize::setDelay(2000);
+					customize::clearScreen();
+					return;
+				}
+			}
+		}
+
+		customize::setColor(12); // red
+		std::cout << "Task with ID " << id << " not found.\n\n";
+		customize::setColor(7);
+		customize::setDelay(2000);
+		customize::clearScreen();
+	}
+
 }
 
 void removeTask() //funtion to remove a task
 {
+	customize::setDelay(1200);
+	customize::clearScreen();
+	if (taskList.empty())
+	{
+		customize::setColor(12); // red
+		std::cout << "No tasks available to remove.\n\n";
+		customize::setColor(7);
+		customize::setDelay(2000);
+		customize::clearScreen();
+		return;
+	}
+	else
+	{
+		std::cout << "Enter the ID of the task to remove: ";
+		int id{};
+		std::cin >> id;
+		if (!std::cin)
+		{
+			cinError();
+			customize::setColor(12); // red
+			std::cout << "Invalid input. Please enter a valid task ID.\n\n";
+			customize::setColor(7);
+			customize::setDelay(2000);
+			customize::clearScreen();
+			return;
+		}
+		if (id < 0) // checks if the id is negative
+		{
+			customize::setColor(12); // red
+			std::cout << "Invalid ID. Task ID cannot be negative.\n\n";
+			customize::setColor(7);
+			customize::setDelay(2000);
+			customize::clearScreen();
+			return;
+		}
+
+		for (auto& task : taskList) // accesses each task in the task list
+		{
+			if (task.getId() == id) // checks if the task ID matches the input ID
+			{
+				customize::setColor(14); // yellow
+				std::cout << "Removing task " << task.getName() << "...\n";
+				taskList.erase(taskList.begin() + id); // removes the task from the list
+				customize::setDelay(1000);
+				customize::clearScreen();
+				customize::setColor(10); // green
+				std::cout << "Task removed successfully!\n\n";
+				customize::setColor(7);
+				customize::setDelay(2000);
+				customize::clearScreen();
+				return;
+			}
+		}
+	}
 }
 
 void saveTaskList() //funtion to save the task list to a file
 {
-	std::this_thread::sleep_for(std::chrono::milliseconds(1200));
-	clearScreen();
+	customize::setDelay(1200); // delay for 1.2 seconds
+	customize::clearScreen();
 	if (taskList.empty())
 	{
-		setColor(12); // red
+		customize::setColor(12); // red
 		std::cout << "No tasks to save.\n\n";
-		setColor(7);
-		std::this_thread::sleep_for(std::chrono::seconds(2));
-		clearScreen();
+		customize::setColor(7);
+		customize::setDelay(2000); // delay for 2 seconds
+		customize::clearScreen();
 		return;
 	}
 
 	if (saveTaskListInFile(taskList, "task.txt"))
 	{
-		setColor(10); // green
+		customize::setColor(10); // green
 		std::cout << "Task list saved successfully!\n\n";
-		setColor(7);
+		customize::setColor(7);
 	}
 	else
 	{
-		setColor(12); // red
+		customize::setColor(12); // red
 		std::cout << "Failed to save task list.\n\n";
-		setColor(7);
+		customize::setColor(7);
 	}
 
-	std::this_thread::sleep_for(std::chrono::seconds(2));
-	clearScreen();
+	customize::setDelay(2000); // delay for 2 seconds
+	customize::clearScreen();
 }
 
 int main()
@@ -186,50 +334,56 @@ int main()
 		if (!std::cin) // checks if the input is valid
 		{
 			cinError();
-			setColor(12); // red
+			customize::setColor(12); // red
 			std::cout << "Invalid input. Try again.\n\n";
-			setColor(7);
+			customize::setColor(7);
+			customize::setDelay(1000);
+			customize::clearScreen();
 			continue;
 		}
 
 		switch (option)
 		{
 		case 1:
-			clearScreen();
+			customize::clearScreen();
 			std::cout << "Add task selected.\n\n";
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // clear the input buffer
 			addTask();
 			break;
 		case 2:
-			clearScreen();
+			customize::clearScreen();
 			std::cout << "List tasks selected.\n\n";
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 			listTasks();
 			break;
 		case 3:
-			clearScreen();
+			customize::clearScreen();
 			std::cout << "Mark task as complete selected.\n\n";
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 			markTaskAsCompleted();
 			break;
 		case 4:
-			clearScreen();
+			customize::clearScreen();
 			std::cout << "Remove task selected.\n\n";
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 			removeTask();
 			break;
 		case 5:
-			clearScreen();
+			customize::clearScreen();
 			std::cout << "Save list selected.\n\n";
 			saveTaskList();
 			break;
 		case 6:
 		case 0:
 			std::cout << "Leaving the program...\n\n";
-			std::this_thread::sleep_for(std::chrono::seconds(1)); 
+			customize::setDelay(1000);
 			isRunning = false;
 			break;
 
 		default:
-			setColor(12); // red
+			customize::setColor(12); // red
 			std::cout << "Invalid option. Try again\n\n";
-			setColor(7);
+			customize::setColor(7);
 			cinError();
 			continue;
 		}
